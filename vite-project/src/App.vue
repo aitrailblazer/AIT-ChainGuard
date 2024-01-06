2b541f6c7a50fa5245c38cc418e929ae4219a4b6
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2024-01-05 23:03:06 -0800

    docs

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -428,7 +428,7 @@ async function readTransaction() {
     </section>
 
     <!-- Section: Buttons for Permanent Storage and Data Retrieval -->
-    <section v-if="transactionId" class="w-full max-w-2xl mt-10">
+    <section  class="w-full max-w-2xl mt-10">
       <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="buttons">
           <!-- Button: Store Data Permanently on Arweave -->
